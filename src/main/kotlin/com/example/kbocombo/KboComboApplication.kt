144b19282a4244ead393b6216f08f77f0c460406
gray <68818952+Kim0914@users.noreply.github.com>
2025-01-05 16:46:49 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kbocombo/KboComboApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kbocombo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KboComboApplication
+
+fun main(args: Array<String>) {
+    runApplication<KboComboApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kbocombo/KboComboApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kbocombo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KboComboApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
