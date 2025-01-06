d75b9440f49b194f668b0680d980cf5c6ba4eee7
carsago <ohseo8813@gmail.com>
2025-01-06 18:54:01 +0900

    refactor: 네이밍 변경

---
+++ src/main/kotlin/com/example/kbocombo/crawler/KboSiteParameterGenerator.kt
@@ -0,0 +1,64 @@
+package com.example.kbocombo.crawler
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+
+@Component
+class KboSiteParameterGenerator {
+
+    fun getParams(response: ResponseEntity<String>): MultiValueMap<String, String> {
+        val formData = LinkedMultiValueMap<String, String>()
+        val body = response.body ?: throw IllegalArgumentException("Initial body is null")
+        setDefaultParams(formData, response.body) { key -> getStateById(key, Jsoup.parse(body)) }
+        formData.add(EVENT_TARGET, BUTTON)
+        return formData
+    }
+
+    private fun setDefaultParams(
+        formData: MultiValueMap<String, String>,
+        body: String?,
+        getState: (String) -> String
+    ) {
+        formData.add(SCRIPT_MANAGER_KEY, SCRIPT_MANAGER_VALUE)
+        formData.add(SELECT_SEARCH_DATE, getDate(body ?: throw IllegalStateException("KBO Stats Body is null")))
+        addFormData(formData, VIEW_STATE, getState)
+        addFormData(formData, VIEW_STATE_GENERATOR, getState)
+        addFormData(formData, EVENT_VALIDATION, getState)
+        formData.add("__ASYNCPOST", "true")
+    }
+
+    private fun addFormData(formData: MultiValueMap<String, String>, key: String, getValue: (String) -> String) {
+        formData.add(key, getValue(key))
+    }
+
+    private fun getStateById(key: String, document: Document): String {
+        return document.getElementById(key)?.attr("value")
+            ?: throw IllegalStateException("State By Id is null")
+    }
+
+    private fun getDate(body: String): String {
+        return Jsoup.parse(body)
+            .getElementById(DATE_ID)
+            .attribute("value")
+            .value
+    }
+
+    companion object {
+        private const val DATE_ID = "cphContents_cphContents_cphContents_hfSearchDate"
+        private const val PREFIX = "ctl00\$ctl00\$ctl00\$cphContents\$cphContents\$cphContents\$"
+        private const val RECORD = "udpRecord|"
+        const val SELECT_TEAM_KEY = "${PREFIX}hfSearchTeam"
+        private const val SELECT_SEARCH_DATE = "${PREFIX}hfSearchDate"
+        private const val SCRIPT_MANAGER_KEY = "${PREFIX}ScriptManager1"
+        private const val BUTTON = "${PREFIX}btnCalendarSelect"
+        private const val SCRIPT_MANAGER_VALUE = "${PREFIX}${RECORD}${BUTTON}"
+        private const val EVENT_TARGET = "__EVENTTARGET"
+        private const val VIEW_STATE = "__VIEWSTATE"
+        private const val VIEW_STATE_GENERATOR = "__VIEWSTATEGENERATOR"
+        private const val EVENT_VALIDATION = "__EVENTVALIDATION"
+    }
+}
