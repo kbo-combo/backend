f7800d3645d877d91041415b52f9e302e067835e
carsago <ohseo8813@gmail.com>
2025-01-06 18:24:06 +0900

    feat: kboClient 추가

---
+++ src/main/kotlin/com/example/kbocombo/crawler/KboClient.kt
@@ -0,0 +1,19 @@
+package com.example.kbocombo.crawler
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.service.annotation.HttpExchange
+import org.springframework.web.service.annotation.PostExchange
+
+@HttpExchange
+interface KboClient {
+
+    @PostExchange("/Player/Register.aspx")
+    fun getRegisteredPlayers(
+        @RequestBody form: MultiValueMap<String, String>,
+        @RequestHeader(HttpHeaders.COOKIE, required = false) cookie: String?
+    ): ResponseEntity<String>
+}
---
+++ src/main/kotlin/com/example/kbocombo/crawler/KboClientConfig.kt
@@ -0,0 +1,35 @@
+package com.example.kbocombo.crawler
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.support.RestClientAdapter
+import org.springframework.web.service.invoker.HttpServiceProxyFactory
+import java.util.Collections
+
+@Configuration
+class KboClientConfig {
+
+    @Bean
+    fun kboClient() : KboClient {
+        val client = RestClient.builder()
+            .baseUrl("https://www.koreabaseball.com")
+            .defaultHeaders { it.addAll(getHeaders())}
+            .build()
+        val adapter = RestClientAdapter.create(client)
+        val factory = HttpServiceProxyFactory.builderFor(adapter)
+            .build()
+        return factory.createClient(KboClient::class.java)
+    }
+
+    fun getHeaders(): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+        headers.accept = Collections.singletonList(MediaType.ALL)
+        headers.add("Cache-Control", "no-cache")
+        headers.add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36");
+        return headers
+    }
+}
