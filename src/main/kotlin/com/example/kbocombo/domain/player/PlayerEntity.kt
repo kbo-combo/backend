844a72147eb31d03d29b4f2f96726eed5cbcc35d
carsago <ohseo8813@gmail.com>
2025-01-06 21:23:47 +0900

    refactor: entity 필드 개행 변경

--- src/main/kotlin/com/example/kbocombo/domain/player/PlayerEntity.kt
+++ src/main/kotlin/com/example/kbocombo/domain/player/PlayerEntity.kt
@@ -18,28 +18,40 @@ import java.time.LocalDate
 class PlayerEntity(
     @Column(name = "birth_date", nullable = false)
     val birthDate: LocalDate,
+
     @Column(name = "height")
     var height: Int?,
+
     @Column(name = "weight")
     var weight: Int?,
+
     @Column(name = "draft_info")
     var draftInfo: String?,
+
     @Column(name = "draft_year")
     var draftYear: Int?,
+
     @Column(name = "hitting_hand", nullable = false)
     var hittingHand: HandType,
+
     @Column(name = "throwing_hand", nullable = false)
     var throwingHand: HandType,
+
     @Column(name = "name", nullable = false)
     val name: String,
+
     @Column(name = "web_id", nullable = false)
     val webId: WebId,
+
     @Column(name = "position")
     var position: PlayerPosition?,
+
     @Column(name = "detail_position")
     var detailPosition: PlayerDetailPosition?,
+
     @Column(name = "team", nullable = false)
     var team: Team,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
