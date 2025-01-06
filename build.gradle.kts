63ef08e4be8fd2970be5c1ed9009609ca5d3024d
carsago <ohseo8813@gmail.com>
2025-01-06 17:26:55 +0900

    feat: Jsoup 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,15 @@ repositories {
     mavenCentral()
 }
 
+val jsoupVersion = "1.17.2"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jsoup:jsoup:$jsoupVersion")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
