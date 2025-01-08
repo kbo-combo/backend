7a597ad1330039ef2d11bf2d3580d4ec23e69f04
carsago <ohseo8813@gmail.com>
2025-01-09 00:44:33 +0900

    feat: PlayerImage 추가

--- src/main/kotlin/com/example/kbocombo/domain/player/PlayerImage.kt
+++ src/main/kotlin/com/example/kbocombo/domain/player/PlayerImage.kt
@@ -2,12 +2,9 @@ package com.example.kbocombo.domain.player
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 
 @Table(name = "PLAYER_IMAGE")
@@ -17,12 +14,11 @@ class PlayerImage(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "player_id", nullable = false)
-    val player: Player,
-
     @Column(name = "image_url")
     private var imageUrl: String? = null,
+
+    @Column(name = "player_id")
+    val playerId: Long
 ) {
 
     fun getProfileImageUrl(): String? {
