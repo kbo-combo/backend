c5daba22ed3bef6a6b347f14a62c4c9537e21ad7
gray <68818952+Kim0914@users.noreply.github.com>
2025-01-11 20:36:43 +0900

    카카오 로그인 구현 (#3)
    * feat: 소셜 로그인 클라이언트 구현
    
    * feat: 소셜 로그인 provider를 식별하는 enum 객체 추가
    
    * feat: Member 엔티티 생성
    
    * feat: 카카오 OAuth 클라이언트 생성
    
    * feat: 서브모듈 설정
    
    * feat: 서브모듈 설정 값 반영
    
    * feat: OAuth 클라이언트를 관리하는 객체 추가
    
    * feat: 카카오 로그인 응답을 받는 객체 추가
    
    * feat: 카카오 로그인 기능 추가
    
    * build: h2 인메모리 의존성 추가
    
    * refactor: 코드 정리
    
    * refactor: OAuthClients를 직접 빈으로 등록하도록 변경
    
    * refactor: 카카오 회원 정보를 받아올 때 닉네임 빼고 받기
    
    * refactor: redirect URI 반환하도록 변경
    
    * refactor: RestClient 사용하도록 변경
    
    * fix: baseUrl 수정
    
    * refactor: 카카오 멤버 조회 RestClient로 변경
    
    * refactor: read only 트랜잭션 제거

---
+++ src/main/kotlin/com/example/kbocombo/auth/presentation/AuthController.kt
@@ -0,0 +1,47 @@
+package com.example.kbocombo.auth.presentation
+
+import com.example.kbocombo.auth.application.AuthService
+import com.example.kbocombo.auth.application.OAuthMemberResponse
+import com.example.kbocombo.auth.application.OAuthRedirectUriResponse
+import com.example.kbocombo.member.domain.vo.SocialProvider
+import jakarta.servlet.http.HttpServletResponse
+import java.util.Locale
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin(value = ["*"])
+class AuthController(
+    private val authService: AuthService
+) {
+    @GetMapping("/oauth/{socialProvider}")
+    fun getAuthRedirectUri(
+        @PathVariable socialProvider: String,
+        @RequestParam redirectUri: String,
+        response: HttpServletResponse
+    ): ResponseEntity<OAuthRedirectUriResponse> {
+        val oAuthRedirectUriResponse = authService.getRedirectUri(
+            socialProvider = SocialProvider.valueOf(socialProvider.uppercase(Locale.getDefault())),
+            redirectUri = redirectUri
+        )
+        return ResponseEntity.ok().body(oAuthRedirectUriResponse)
+    }
+
+    @GetMapping("/oauth/{socialProvider}/login")
+    fun login(
+        @PathVariable socialProvider: String,
+        @RequestParam code: String,
+        @RequestParam redirectUri: String,
+    ): ResponseEntity<OAuthMemberResponse> {
+        val result = authService.getMemberInfo(
+            socialProvider = SocialProvider.valueOf(socialProvider.uppercase(Locale.getDefault())),
+            code = code,
+            redirectUri = redirectUri
+        )
+        return ResponseEntity.ok().body(result)
+    }
+}
---
+++ src/main/kotlin/com/example/kbocombo/member/domain/Member.kt
@@ -0,0 +1,32 @@
+package com.example.kbocombo.member.domain
+
+import com.example.kbocombo.member.domain.vo.SocialProvider
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Table(name = "MEMBER")
+@Entity
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(name = "email", nullable = false, updatable = false)
+    val email: String,
+
+    @Column(name = "nickname", nullable = false)
+    val nickname: String,
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "social_provider")
+    val socialProvider: SocialProvider?,
+
+    @Column(name = "social_id")
+    val socialId: String?
+)
