504e407dfd9591b7fab26e468cf47367e6db3420
gray <kosa0914@naver.com>
2025-01-11 14:49:28 +0900

    refactor: RestClient 사용하도록 변경

---
+++ src/main/kotlin/com/example/kbocombo/auth/config/OAuthHttpConfig.kt
@@ -0,0 +1,23 @@
+package com.example.kbocombo.auth.config
+
+import com.example.kbocombo.auth.infra.KakaoHttpClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.support.RestClientAdapter
+import org.springframework.web.service.invoker.HttpServiceProxyFactory
+
+@Configuration
+class OAuthHttpConfig {
+
+    @Bean
+    fun kakaoHttpClient(): KakaoHttpClient {
+        val client = RestClient.builder()
+            .baseUrl("https://www.koreabaseball.com")
+            .build()
+        val adapter = RestClientAdapter.create(client)
+        val factory = HttpServiceProxyFactory.builderFor(adapter)
+            .build()
+        return factory.createClient(KakaoHttpClient::class.java)
+    }
+}
--- src/main/kotlin/com/example/kbocombo/auth/infra/KakaoLoginClient.kt
+++ src/main/kotlin/com/example/kbocombo/auth/infra/KakaoLoginClient.kt
@@ -3,26 +3,20 @@ package com.example.kbocombo.auth.infra
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.UnknownContentTypeException
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
 import org.springframework.web.util.UriComponentsBuilder
-import java.nio.charset.StandardCharsets
 
 @Component
 class KakaoLoginClient(
     @Value("\${kakao.client_id}")
-    private val clientId: String
+    private val clientId: String,
+    private val kakaoHttpClient: KakaoHttpClient
 ) {
-    private val restTemplate: RestTemplate = RestTemplate()
-
     fun createRedirectUri(redirectUrl: String): String {
         return UriComponentsBuilder.fromUriString(AUTHORIZE_REQUEST_URL)
             .queryParam("client_id", clientId)
@@ -33,45 +27,33 @@ class KakaoLoginClient(
     }
 
     fun getAccessToken(authCode: String, redirectUri: String): String {
-        val headers = HttpHeaders().apply {
-            contentType = MediaType.APPLICATION_FORM_URLENCODED
-            acceptCharset = listOf(StandardCharsets.UTF_8)
-        }
-
-        val body = LinkedMultiValueMap<String, String>().apply {
-            add("grant_type", GRANT_TYPE)
-            add("client_id", clientId)
-            add("redirect_uri", redirectUri)
-            add("code", authCode)
-        }
-
-        val requestEntity = HttpEntity(body, headers)
-
-        val responseEntity = restTemplate.exchange(
-            ACCESS_TOKEN_REQUEST_URL,
-            HttpMethod.POST,
-            requestEntity,
-            KakaoTokenResponse::class.java
+        val kakaoTokenResponse = kakaoHttpClient.getAccessToken(
+            grantType = GRANT_TYPE,
+            clientId = clientId,
+            redirectUri = redirectUri,
+            authCode = authCode
         )
-
-        val kakaoTokenResponse = responseEntity.body
-            ?: throw IllegalStateException("카카오 로그인 토큰 발급에 실패했습니다.")
 
         return kakaoTokenResponse.accessToken
     }
 
     companion object {
         private const val AUTHORIZE_REQUEST_URL: String = "https://kauth.kakao.com/oauth/authorize"
-        private const val ACCESS_TOKEN_REQUEST_URL: String = "https://kauth.kakao.com/oauth/token"
         private const val GRANT_TYPE: String = "authorization_code"
         private const val RESPONSE_TYPE: String = "code"
     }
 }
 
 @HttpExchange
-interface KakaoRestClient {
+interface KakaoHttpClient {
 
-    @PostExchange("/oauth/token",)
+    @PostExchange(
+        url = "/oauth/token",
+        headers = [
+            "Content-Type=application/x-www-form-urlencoded",
+            "Accept-Charset=UTF-8"
+        ]
+    )
     fun getAccessToken(
         @RequestParam("grant_type") grantType: String,
         @RequestParam("client_id") clientId: String,
