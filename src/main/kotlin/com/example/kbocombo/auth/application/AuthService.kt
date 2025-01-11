0889ab77f48c4564f986527c78ad42407395c30d
gray <kosa0914@naver.com>
2025-01-11 18:00:05 +0900

    refactor: read only 트랜잭션 제거

--- src/main/kotlin/com/example/kbocombo/auth/application/AuthService.kt
+++ src/main/kotlin/com/example/kbocombo/auth/application/AuthService.kt
@@ -4,10 +4,8 @@ import com.example.kbocombo.auth.domain.MemberInfo
 import com.example.kbocombo.auth.infra.OAuthClients
 import com.example.kbocombo.member.domain.vo.SocialProvider
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
 class AuthService(
     private val oAuthClients: OAuthClients
 ) {
