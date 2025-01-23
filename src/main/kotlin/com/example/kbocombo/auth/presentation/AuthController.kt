c7e76f551cda62db7234ab2f90a244458a843e44
gray <kosa0914@naver.com>
2025-01-23 20:32:14 +0900

    style: 불필요한 import 제거

--- src/main/kotlin/com/example/kbocombo/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/kbocombo/auth/presentation/AuthController.kt
@@ -5,14 +5,13 @@ import com.example.kbocombo.auth.application.OAuthMemberResponse
 import com.example.kbocombo.auth.application.OAuthRedirectUriResponse
 import com.example.kbocombo.member.domain.vo.SocialProvider
 import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
+import java.util.Locale
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.Locale
 
 
 @RestController
