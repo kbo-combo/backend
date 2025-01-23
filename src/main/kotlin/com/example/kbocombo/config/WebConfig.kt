aad023bf2ef24b61aa8ea42c9598fe624bca64bf
gray <kosa0914@naver.com>
2025-01-23 20:31:50 +0900

    refactor: cors 전역으로 설정

--- src/main/kotlin/com/example/kbocombo/config/WebConfig.kt
+++ src/main/kotlin/com/example/kbocombo/config/WebConfig.kt
@@ -3,6 +3,7 @@ package com.example.kbocombo.config
 import com.example.kbocombo.member.ui.MemberArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -14,5 +15,16 @@ class WebConfig(
         resolvers.add(
             memberArgumentResolver
         )
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "https://www.kbo-dev.kro.kr",
+                "https://localhost:5173",
+                "https://localhost:5174"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowCredentials(true)
     }
 }
