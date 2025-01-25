bde0364bcf7ef4e7d0cf76f721253902ed18eb67
carsago <ohseo8813@gmail.com>
2025-01-25 15:12:51 +0900

    feat: Game Entity 추가

---
+++ src/main/kotlin/com/example/kbocombo/game/domain/Game.kt
@@ -0,0 +1,34 @@
+package com.example.kbocombo.game.domain
+
+import com.example.kbocombo.game.domain.vo.GameType
+import com.example.kbocombo.player.vo.Team
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity(name = "GAME")
+class Game(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "home_team", nullable = false)
+    val homeTeam: Team,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "away_team", nullable = false)
+    val awayTeam: Team,
+
+    @Column(name = "start_datetime", nullable = false)
+    val startDateTime: LocalDateTime,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "game_type", nullable = false)
+    val gameType: GameType,
+)
---
+++ src/main/kotlin/com/example/kbocombo/game/domain/vo/GameType.kt
@@ -0,0 +1,7 @@
+package com.example.kbocombo.game.domain.vo
+
+enum class GameType {
+    PRE_SEASON,
+    REGULAR_SEASON,
+    POST_SEASON
+}
