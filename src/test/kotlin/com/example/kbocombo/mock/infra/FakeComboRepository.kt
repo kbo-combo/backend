619399196f6783df2f306bfffc366417d6d899f0
carsago <ohseo8813@gmail.com>
2025-01-27 22:42:58 +0900

    refactor: 사용하지 않는 코드 제거

--- src/test/kotlin/com/example/kbocombo/mock/infra/FakeComboRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kbocombo.mock.infra
-
-import com.example.kbocombo.combo.domain.Combo
-import com.example.kbocombo.combo.infra.ComboRepository
-
-class FakeComboRepository : BaseFakeRepository<Combo>(Combo::class), ComboRepository {
-
-
-}
