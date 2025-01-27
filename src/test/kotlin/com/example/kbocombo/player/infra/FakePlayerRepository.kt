596ed31e45af44b021785210811e266ede879030
carsago <ohseo8813@gmail.com>
2025-01-28 00:17:56 +0900

    refactor: 테스트 코드 컴파일 에러 수정

--- src/test/kotlin/com/example/kbocombo/player/infra/FakePlayerRepository.kt
+++ src/test/kotlin/com/example/kbocombo/player/infra/FakePlayerRepository.kt
@@ -3,9 +3,13 @@ package com.example.kbocombo.player.infra
 import com.example.kbocombo.mock.infra.BaseFakeRepository
 import com.example.kbocombo.player.Player
 
-class FakePlayerRepository : BaseFakeRepository<Player>(Player::class), PlayerRepository {
+ class FakePlayerRepository : BaseFakeRepository<Player>(Player::class), PlayerRepository {
 
     override fun findAllByIsRetiredFalse(): List<Player> {
         return db.filterNot { it.isRetired }
     }
-}
+
+     override fun findById(playerId: Long): Player? {
+         return db.find { it.id == playerId }
+     }
+ }
