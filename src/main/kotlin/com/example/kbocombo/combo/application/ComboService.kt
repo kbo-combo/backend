5654de93276a6b75a8334a3b483e8c3da14ec8c6
carsago <ohseo8813@gmail.com>
2025-01-27 23:00:08 +0900

    feat: 콤보 생성 기능 추가

--- src/main/kotlin/com/example/kbocombo/combo/application/ComboService.kt
+++ src/main/kotlin/com/example/kbocombo/combo/application/ComboService.kt
@@ -1,8 +1,13 @@
 package com.example.kbocombo.combo.application
 
+import com.example.kbocombo.combo.domain.Combo
 import com.example.kbocombo.combo.infra.ComboRepository
 import com.example.kbocombo.combo.infra.getById
+import com.example.kbocombo.combo.ui.request.ComboCreateRequest
 import com.example.kbocombo.game.infra.GameRepository
+import com.example.kbocombo.game.infra.getById
+import com.example.kbocombo.player.infra.PlayerRepository
+import com.example.kbocombo.player.infra.getById
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -11,8 +16,21 @@ import java.time.LocalDateTime
 @Transactional
 class ComboService(
     private val gameRepository: GameRepository,
+    private val playerRepository: PlayerRepository,
     private val comboRepository: ComboRepository,
 ) {
+
+    fun createCombo(request: ComboCreateRequest, memberId: Long, now: LocalDateTime) {
+        val game = gameRepository.getById(request.gameId)
+        val player = playerRepository.getById(request.playerId)
+        val combo = Combo(
+            game = game,
+            memberId = memberId,
+            playerId = player.id,
+            now = now,
+        )
+        comboRepository.save(combo)
+    }
 
     fun deleteCombo(comboId: Long, now: LocalDateTime) {
         val combo = comboRepository.getById(comboId)
