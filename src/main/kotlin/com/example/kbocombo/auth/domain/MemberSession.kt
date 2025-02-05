cd088d6e9d9c83d49f1b58f4c051f14b7cca1fad
carsago <ohseo8813@gmail.com>
2025-02-05 20:51:19 +0900

    feat: MemberSession 추가

---
+++ src/main/kotlin/com/example/kbocombo/auth/domain/MemberSession.kt
@@ -0,0 +1,25 @@
+package com.example.kbocombo.auth.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity(name = "MEMBER_SESSION")
+class MemberSession(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(name = "member_id", nullable = false, updatable = false)
+    val memberId: Long,
+
+    @Column(name = "session_key", nullable = false, updatable = false, unique = true)
+    val sessionKey: String,
+
+    @Column(name = "expired_datetime", nullable = false, updatable = false)
+    var expiredDateTime: LocalDateTime,
+ ) {
+}
