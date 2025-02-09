35e591555797aa4bc20bec2384ec459a11bccb52
carsago <ohseo8813@gmail.com>
2025-02-09 16:47:30 +0900

    feat: Test 코드 추가

---
+++ src/test/kotlin/com/example/kbocombo/crawler/infra/NaverSportGameHandlerTest.kt
@@ -0,0 +1,27 @@
+package com.example.kbocombo.crawler.infra
+
+import io.kotest.core.annotation.Ignored
+import io.kotest.core.spec.style.FunSpec
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Ignored
+@DisplayNameGeneration(ReplaceUnderscores::class)
+@SpringBootTest
+class NaverSportGameHandlerTest(
+    private val sut: NaverSportGameHandler
+) : FunSpec({
+
+    context("Game을 저장한다") {
+        var date = LocalDate.parse("2025-03-08")
+        val endDate = LocalDate.parse("2025-11-01")
+
+        while (date.isBefore(endDate) || date.isEqual(endDate)) {
+            sut.renewGames(gameDate = date, now = LocalDateTime.now())
+        }
+    }
+})
+
