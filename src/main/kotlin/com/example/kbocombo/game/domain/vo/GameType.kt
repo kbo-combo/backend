91faa05cd0e617b2ab1de5bb8b36be2f1b50c44e
carsago <ohseo8813@gmail.com>
2025-02-09 15:16:30 +0900

    feat: GameType으로 GameDate 반환

--- src/main/kotlin/com/example/kbocombo/game/domain/vo/GameType.kt
+++ src/main/kotlin/com/example/kbocombo/game/domain/vo/GameType.kt
@@ -1,7 +1,23 @@
 package com.example.kbocombo.game.domain.vo
 
+import java.time.LocalDate
+
 enum class GameType {
     PRE_SEASON,
     REGULAR_SEASON,
-    POST_SEASON
+    POST_SEASON;
+
+    companion object {
+
+        private val REGULAR_SEASON_DATE = LocalDate.parse("2025-03-28")
+
+        fun getGameTypeByDate(gameDate: LocalDate): GameType {
+            require(gameDate.year == 2025) { "게임연도가 2025년이 아닙니다."}
+            if (gameDate >= REGULAR_SEASON_DATE) {
+                return REGULAR_SEASON
+            }
+
+            return PRE_SEASON
+        }
+    }
 }
