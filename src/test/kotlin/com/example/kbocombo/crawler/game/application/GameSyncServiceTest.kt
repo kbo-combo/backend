930a5fecd97c65da1803970df0dd839889fec6f2
carsago <ohseo8813@gmail.com>
2025-02-09 18:12:12 +0900

    refactor: 테스트코드 범위 수정

--- src/test/kotlin/com/example/kbocombo/crawler/game/application/GameSyncServiceTest.kt
+++ src/test/kotlin/com/example/kbocombo/crawler/game/application/GameSyncServiceTest.kt
@@ -16,11 +16,12 @@ class GameSyncServiceTest(
 ) : FunSpec({
 
     context("Game을 저장한다") {
-        var date = LocalDate.parse("2025-03-08")
-        val endDate = LocalDate.parse("2025-11-01")
+        var date = LocalDate.parse("2024-03-01")
+        val endDate = LocalDate.parse("2025-10-02")
 
         while (date.isBefore(endDate) || date.isEqual(endDate)) {
             sut.syncGame(gameDate = date, now = LocalDateTime.now())
+            date = date.plusDays(1)
         }
     }
 })
