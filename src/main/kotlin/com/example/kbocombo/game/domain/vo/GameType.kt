9c01eb19693332805887a9596e1b1dc0fc2a8e08
carsago <ohseo8813@gmail.com>
2025-02-09 15:47:53 +0900

    refactor: 정규시즌 Date 변경

--- src/main/kotlin/com/example/kbocombo/game/domain/vo/GameType.kt
+++ src/main/kotlin/com/example/kbocombo/game/domain/vo/GameType.kt
@@ -9,7 +9,7 @@ enum class GameType {
 
     companion object {
 
-        private val REGULAR_SEASON_DATE = LocalDate.parse("2025-03-28")
+        private val REGULAR_SEASON_DATE = LocalDate.parse("2025-03-22")
 
         fun getGameTypeByDate(gameDate: LocalDate): GameType {
             require(gameDate.year == 2025) { "게임연도가 2025년이 아닙니다."}
