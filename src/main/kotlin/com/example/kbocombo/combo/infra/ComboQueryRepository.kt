59f5471816c984bb0dafcdaeeb382ee66b4d778c
carsago <ohseo8813@gmail.com>
2025-02-11 22:39:09 +0900

    feat: Combo 조회 쿼리 추가

---
+++ src/main/kotlin/com/example/kbocombo/combo/infra/ComboQueryRepository.kt
@@ -0,0 +1,38 @@
+package com.example.kbocombo.combo.infra
+
+import com.example.kbocombo.combo.domain.QCombo.combo
+import com.example.kbocombo.combo.domain.vo.ComboStatus
+import com.example.kbocombo.player.QPlayer.player
+import com.querydsl.core.annotations.QueryProjection
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
+import java.time.LocalDate
+
+@Repository
+class ComboQueryRepository(
+    private val queryFactory: JPAQueryFactory
+) {
+
+    fun findByGameDate(comboDate: LocalDate): ComboResponse? {
+        return queryFactory
+            .select(QComboResponse(
+                combo.id,
+                player.id,
+                player.name,
+                player.playerImage.imageUrl,
+                combo.comboStatus
+            ))
+            .from(combo)
+            .leftJoin(player).on(player.id.eq(combo.playerId))
+            .where(combo.gameDate.eq(comboDate))
+            .fetchOne()
+    }
+}
+
+data class ComboResponse @QueryProjection constructor(
+    val comboId: Long,
+    val playerId: Long,
+    val playerName: String,
+    val playerImageUrl: String?,
+    val comboStatus: ComboStatus
+)
