ef2e0d2df138a1d826872429074b12f26f10a1d0
carsago <ohseo8813@gmail.com>
2025-02-12 22:39:28 +0900

    feat: @RestControllerAdvice 추가

--- src/main/kotlin/com/example/kbocombo/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/example/kbocombo/exception/CustomExceptionHandler.kt
@@ -4,8 +4,10 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
+@RestControllerAdvice
 class CustomExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(AuthenticationException::class)
