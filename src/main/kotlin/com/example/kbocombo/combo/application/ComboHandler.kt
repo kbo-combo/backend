2ad6f0bba39a4744aab1be192ee3c180789c145f
gray <kosa0914@naver.com>
2025-02-16 17:25:55 +0900

    feat: 경기 종료 이벤트가 발행되면 안타를 치지 않은 콤보는 실패되는 트리거 추가

--- src/main/kotlin/com/example/kbocombo/combo/application/ComboHandler.kt
+++ src/main/kotlin/com/example/kbocombo/combo/application/ComboHandler.kt
@@ -20,4 +20,21 @@ class ComboHandler(
 
         comboService.updateComboToSuccess(gameId = gameId, playerWebId = playerCode.toLong())
     }
+
+    /**
+     * 경기 종료 트리거를 어떻게 할 지 생각해봐야 함.
+     * 경기 종료가 되자 마자 실행하는 것보다, 일정 시간의 여유를 두고 실행하는게 좋을듯.
+     * GameEndedEvent 클래스를 마땅히 둘 곳이 없어서, 우선 이 클래스에 생성합니다.
+     * -> 추후 이벤트 발행하는 곳으로 옮겨야 함
+     */
+    @Async
+    @EventListener
+    fun handleGameEndedEvent(gameEndedEvent: GameEndedEvent) {
+        val gameId = gameEndedEvent.gameId
+        logInfo("Game ended: $gameId ")
+
+        comboService.updateComboToFail(gameId = gameId)
+    }
 }
+
+data class GameEndedEvent(val gameId: Long)
