ae2fbb4186f6b606bca4aa8fbfa750b1af731ace
carsago <ohseo8813@gmail.com>
2025-03-03 15:22:16 +0900

    feat: SliceResponse 추가

---
+++ src/main/kotlin/com/example/kbocombo/common/dto/SliceResponse.kt
@@ -0,0 +1,15 @@
+package com.example.kbocombo.common.dto
+
+data class SliceResponse<T> private constructor(
+    val content: List<T>,
+    val hasNext: Boolean
+) {
+
+    companion object {
+        fun <T> of(content: List<T>, pageSize: Long): SliceResponse<T> {
+            return SliceResponse(
+                content = content.take(pageSize.toInt()),
+                hasNext = content.size > pageSize)
+        }
+    }
+}
