b73ee05d28296f24114667a5b1fb0f00a9584bf8
carsago <ohseo8813@gmail.com>
2025-03-06 03:58:49 +0900

    feat: SchedulerConfig 추가

---
+++ src/main/kotlin/com/example/kbocombo/config/SchedulerConfig.kt
@@ -0,0 +1,25 @@
+package com.example.kbocombo.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+@EnableScheduling
+class SchedulerConfig {
+
+    @Bean
+    fun schedulerExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 2
+        executor.maxPoolSize = 5
+        executor.queueCapacity = 100
+        executor.setThreadNamePrefix("scheduler-executor-")
+        executor.setWaitForTasksToCompleteOnShutdown(true)
+        executor.setAwaitTerminationSeconds(30)
+        executor.initialize()
+        return executor
+    }
+}
