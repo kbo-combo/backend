3772a74053df8a498f017c7b97e61f4986705ff0
carsago <ohseo8813@gmail.com>
2025-03-09 16:12:23 +0900

    refactor: HitterGameRecordService.saveOrUpdateHitterRecords() 가독성 개선

--- src/main/kotlin/com/example/kbocombo/record/application/HitterGameRecordService.kt
+++ src/main/kotlin/com/example/kbocombo/record/application/HitterGameRecordService.kt
@@ -1,6 +1,8 @@
 package com.example.kbocombo.record.application
 
+import com.example.kbocombo.game.domain.Game
 import com.example.kbocombo.game.infra.GameRepository
+import com.example.kbocombo.player.domain.Player
 import com.example.kbocombo.player.infra.PlayerRepository
 import com.example.kbocombo.player.vo.WebId
 import com.example.kbocombo.record.domain.HitterGameRecord
@@ -27,32 +29,46 @@ class HitterGameRecordService(
 
     @Transactional
     fun saveOrUpdateHitterRecords(gameCode: String, playerRecordRequest: List<PlayerRequest>) {
-        val requestByWebId = playerRecordRequest.associateBy { it.webId }
         val game = gameRepository.findByGameCode(gameCode)
             ?: throw IllegalArgumentException("존재하지 않는 게임입니다.")
-        if (game.isPending()) {
-            return
-        }
-        val players = playerRepository.findAllByWebIdIn(requestByWebId.keys)
+        if (game.isPending()) return
+
+        val requestByWebId = playerRecordRequest.associateBy { it.webId }
         val hitterGameRecords = hitterGameRecordRepository.findAllByGameId(game.id)
             .associateBy { it.playerId }
-        for (player in players) {
-            val hitterGameRecord = hitterGameRecords[player.id]
+        saveOrUpdate(requestByWebId, hitterGameRecords, game)
+    }
+
+    private fun saveOrUpdate(
+        requestByWebId: Map<WebId, PlayerRequest>,
+        hitterGameRecords: Map<Long, HitterGameRecord>,
+        game: Game
+    ) {
+        val players = playerRepository.findAllByWebIdIn(requestByWebId.keys)
+        players.forEach { player ->
             val request = requestByWebId[player.webId]!!
-            if (hitterGameRecord == null) {
-                hitterGameRecordRepository.save(HitterGameRecord(gameId = game.id,
-                    gameDate = game.startDate,
-                    playerId = player.id,
-                    pa = request.pa,
-                    hit = request.hit,
-                    ))
-                continue
-            }
-            hitterGameRecord.updateStat(pa = request.pa, hit = hitterGameRecord.hit)
+            hitterGameRecords[player.id]
+                ?.let { updateHitterRecord(it, request) }
+                ?: saveHitterRecord(game, player, request)
         }
     }
-}
 
+    private fun updateHitterRecord(hitterGameRecord: HitterGameRecord, request: PlayerRequest) {
+        hitterGameRecord.updateStat(pa = request.pa, hit = hitterGameRecord.hit)
+    }
+
+    private fun saveHitterRecord(game: Game, player: Player, request: PlayerRequest) {
+        hitterGameRecordRepository.save(
+            HitterGameRecord(
+                gameId = game.id,
+                gameDate = game.startDate,
+                playerId = player.id,
+                pa = request.pa,
+                hit = request.hit
+            )
+        )
+    }
+}
 
 
 data class GameDomainCanceledEvent(val gameId: Long)
