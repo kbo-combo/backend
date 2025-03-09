83e61db0aadc1007a3c5056d6bad153349b1655e
carsago <ohseo8813@gmail.com>
2025-03-09 10:35:58 +0900

    refactor: 조회 기준 변경

--- src/main/kotlin/com/example/kbocombo/game/infra/GameEndEventJobRepository.kt
+++ src/main/kotlin/com/example/kbocombo/game/infra/GameEndEventJobRepository.kt
@@ -1,14 +1,13 @@
 package com.example.kbocombo.game.infra
 
 import com.example.kbocombo.game.domain.GameEndEventJob
-import java.time.LocalDate
 import org.springframework.data.repository.Repository
 
 interface GameEndEventJobRepository : Repository<GameEndEventJob, Long> {
 
     fun save(gameEndEventJob: GameEndEventJob): GameEndEventJob
 
-    fun findAllByProcessedAndGameDate(processed: Boolean, gameDate: LocalDate): List<GameEndEventJob>
+    fun findAllByProcessed(processed: Boolean): List<GameEndEventJob>
 
     fun findById(id: Long): GameEndEventJob
 }
