f9b66df0458ac630a6ffc675734db16317d547ba
carsago <ohseo8813@gmail.com>
2025-03-09 15:28:59 +0900

    feat: HitterGameRecord 추가

---
+++ src/main/kotlin/com/example/kbocombo/record/domain/HitterGameRecord.kt
@@ -0,0 +1,30 @@
+package com.example.kbocombo.record.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDate
+
+@Entity(name = "HITTER_GAME_RECORD")
+class HitterGameRecord(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(name = "game_id", nullable = false)
+    val gameId: Long,
+
+    @Column(name = "game_date", nullable = false)
+    val gameDate: LocalDate,
+
+    @Column(name = "player_id", nullable = false)
+    val playerId: Long,
+
+    @Column(name = "pa", nullable = false)
+    val pa: Int,
+
+    @Column(name = "hit", nullable = false)
+    val hit: Int,
+)
