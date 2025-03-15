32c8835ab7a5137be3715c108e4174c50924e535
carsago <ohseo8813@gmail.com>
2025-03-15 12:02:06 +0900

    refactor: 게임이 취소일시 pass 처리하도록 수정

--- src/main/kotlin/com/example/kbocombo/combo/application/ComboService.kt
+++ src/main/kotlin/com/example/kbocombo/combo/application/ComboService.kt
@@ -89,8 +89,8 @@ class ComboService(
     @Transactional
     fun updateComboToPass(gameId: Long) {
         val game = gameRepository.getById(gameId)
-        if (game.isCompleted().not()) {
-            throw IllegalArgumentException("게임이 종료되지 않은 경우, 콤보 패스 처리를 할 수 없습니다.")
+        if (game.isCancelled().not()) {
+            throw IllegalArgumentException("게임이 취소되지 않은 경우, 콤보 패스 처리를 할 수 없습니다.")
         }
 
         val combos = comboRepository.findAllByGame(game = game)
