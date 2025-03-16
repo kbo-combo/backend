4cdfd485c6e51adff8c93ac973ae73c7e7d74c0e
carsago <ohseo8813@gmail.com>
2025-03-16 14:56:23 +0900

    refactor: AggregateBaseEntity updateDateTime 갱신되도록 수정

--- src/main/kotlin/com/example/kbocombo/common/AggrgateBaseEntity.kt
+++ src/main/kotlin/com/example/kbocombo/common/AggrgateBaseEntity.kt
@@ -1,17 +1,23 @@
 package com.example.kbocombo.common
 
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.AbstractAggregateRoot
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class AggregateBaseEntity<T : AggregateBaseEntity<T>> : AbstractAggregateRoot<T>() {
 
     @CreatedDate
-    val createdDateTime: LocalDateTime = LocalDateTime.now()
-
+    @Column(updatable = false)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+        protected set
     @LastModifiedDate
-    val updatedDateTime: LocalDateTime = LocalDateTime.now()
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
+        protected set
 }
