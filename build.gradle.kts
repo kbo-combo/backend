7519c6cb9ce5e05938a04a9ab1052ed16b2b85bc
carsago <ohseo8813@gmail.com>
2025-03-16 15:05:41 +0900

    refactor: 사용하지 않는 kotest 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
     testImplementation ("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation ("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
     testImplementation ("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    implementation ("io.kotest:kotest-framework-datatest:$kotestVersion")
     testImplementation ("io.kotest.extensions:kotest-extensions-spring:1.3.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
